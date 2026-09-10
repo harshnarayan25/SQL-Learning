f4d31123d5ff8bbeaa4c7a1b918d8cceb0dd212f
Harsh Narayan <harshnarayan2502@gmail.com>
2026-09-10 07:44:50 +0530

    SQL Fundamentals

---
+++ SQL Fundamentals/SELECT QUERY/Query02_Write a SQL query to display only the first_name, last_name, and salary of all employees.sql
@@ -0,0 +1,9 @@
+-- Query 02 : Write a SQL query to display only the first_name, last_name, and salary of all employees.
+
+USE MyDatabase
+
+SELECT 
+	first_name,
+	last_name,
+	salary
+FROM employees
