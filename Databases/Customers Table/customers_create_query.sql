fc77d20fb832fed9f7a667713bc52c11d6cb7328
Harsh Narayan <harshnarayan2502@gmail.com>
2026-09-22 07:13:34 +0530

    SQL Learning

--- Databases/Customers Table/customers_create_query.sql
+++ Databases/Customers Table/customers_create_query.sql
@@ -3,9 +3,9 @@ GO
 
 CREATE TABLE customers (
     id INT NOT NULL,
-    first_name  VARCHAR(50) NOT NULL,
-    country     VARCHAR(50),
-    score       INT,
+    first_name VARCHAR(50) NOT NULL,
+    country VARCHAR(50),
+    score INT,
     CONSTRAINT PK_customers PRIMARY KEY (id)
 );
 GO
