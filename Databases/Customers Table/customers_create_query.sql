027a3ed4b5d266e11fe31edb0d2105d414cec20b
Harsh Narayan <harshnarayan2502@gmail.com>
2026-09-22 07:12:44 +0530

    SQL Learning

---
+++ Databases/Customers Table/customers_create_query.sql
@@ -0,0 +1,12 @@
+USE MyDatabase;
+GO
+
+CREATE TABLE customers (
+    id INT NOT NULL,
+    first_name  VARCHAR(50) NOT NULL,
+    country     VARCHAR(50),
+    score       INT,
+    CONSTRAINT PK_customers PRIMARY KEY (id)
+);
+GO
+
---
+++ Databases/Customers Table/customers_insert_data_query.sql
@@ -0,0 +1,12 @@
+USE MyDatabase;
+GO
+
+-- INSERT CUSTOMERS DATA
+
+INSERT INTO customers (id, first_name, country, score) VALUES
+    (1, 'Maria', 'Germany',  350),
+    (2, ' John', 'USA', 900),
+    (3, 'Georg', 'UK', 750),
+    (4, 'Martin', 'Germany', 500),
+    (5, 'Peter', 'USA', 0);
+GO
